58e6cd39f9e3076a4619c709554e99eeda91ca67
Alexandr SaM <alexsam2070@gmail.com>
2023-06-25 13:16:50 -0400

    Attach PDF

--- src/MessageHandler/PurchaseConfirmationNotificationHandler.php
+++ src/MessageHandler/PurchaseConfirmationNotificationHandler.php
@@ -3,6 +3,7 @@
 namespace App\MessageHandler;
 
 use App\Message\PurchaseConfirmationNotification;
+use Mpdf\Mpdf;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Mime\Email;
@@ -16,13 +17,19 @@ class PurchaseConfirmationNotificationHandler
 
     public function __invoke(PurchaseConfirmationNotification $notification)
     {
-        echo 'Emailing contract note to ' . $notification->getOrder()->getBuyer()->getEmail() . '<br>';
+        $mpdf = new Mpdf();
+        $content = "<h1>Contract Note For Order {$notification->getOrder()->getId()}</h1>";
+        $content .= '<p>Total: <b>$1898.75</b></p>';
+
+        $mpdf->writeHTML($content);
+        $contractNotePdf = $mpdf->output('', 'S');
 
         $email = (new Email())
             ->from('sales@stacksapp.com')
             ->to($notification->getOrder()->getBuyer()->getEmail())
             ->subject('Contact note for order ' . $notification->getOrder()->getId())
-            ->text('Here is your contract note');
+            ->text('Here is your contract note')
+            ->attach($contractNotePdf, 'contract-note.pdf');
 
         $this->mailer->send($email);
     }
