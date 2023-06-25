c1e9348c0c8937afb87928d109dc02d9c8c86cc9
Alexandr SaM <alexsam2070@gmail.com>
2023-06-25 10:54:08 -0400

    Create StockTransactionController class

---
+++ src/Controller/StockTransactionController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Message\PurchaseConfirmationNotification;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StockTransactionController extends AbstractController
+{
+    #[Route('/buy', name: 'buy-stocks')]
+    public function buy(MessageBusInterface $bus): Response
+    {
+        $order = new class {
+            public function getBuyer(): object
+            {
+                return new class {
+                    public function getEmail(): string
+                    {
+                        return 'email@example.com';
+                    }
+                };
+            }
+        };
+
+        $bus->dispatch(new PurchaseConfirmationNotification($order));
+
+        return $this->render('stocks/example.html.twig');
+    }
+}
