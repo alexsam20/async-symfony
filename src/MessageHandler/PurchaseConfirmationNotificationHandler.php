5db9530adb1871e52f83e4df4575f7ae8c30eefa
Alexandr SaM <alexsam2070@gmail.com>
2023-06-25 09:16:07 -0400

    Add messenger and create PurchaseConfirmationNotificationHandler class

---
+++ src/MessageHandler/PurchaseConfirmationNotificationHandler.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\MessageHandler;
+
+use App\Message\PurchaseConfirmationNotification;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#{AsMessageHandler}
+class PurchaseConfirmationNotificationHandler
+{
+    public function __invoke(PurchaseConfirmationNotification $notification)
+    {
+        echo 'Create a PDF contract note...<br>';
+
+        echo 'Emailing contract note to ' . $notification->getOrder()->getBuyer()->getEmail() . '<br>';
+    }
+}
