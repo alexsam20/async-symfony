923c91ed33416993d49baf349bcdbb4b6bd3ef1b
Alexandr SaM <alexsam2070@gmail.com>
2023-06-25 20:12:19 -0400

    Event bus

--- src/Message/PurchaseConfirmationNotification.php
+++ src/Message/PurchaseConfirmationNotification.php
@@ -4,12 +4,4 @@ namespace App\Message;
 
 class PurchaseConfirmationNotification
 {
-    public function __construct(private int|string $orderId)
-    {
-    }
-
-    public function getOrderId(): int|string
-    {
-        return $this->orderId;
-    }
 }
--- src/MessageHandler/Command/SaveOrderHandler.php
+++ src/MessageHandler/Command/SaveOrderHandler.php
@@ -3,14 +3,22 @@
 namespace App\MessageHandler\Command;
 
 use App\Message\Command\SaveOrder;
+use App\Message\Event\OrderSavedEvent;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class SaveOrderHandler implements MessageHandlerInterface
 {
+    public function __construct(private MessageBusInterface $eventBus)
+    {
+    }
+
     public function __invoke(SaveOrder $saveOrder)
     {
         $orderId = 123;
 
         echo 'Order being saved' . PHP_EOL;
+
+        $this->eventBus->dispatch(new OrderSavedEvent($orderId));
     }
 }
